68f52bfd77beea0a7f75d723326397248ce205ba
Ben <bengg2015@gmail.com>
2019-01-25 18:12:48 -0500

    Created Rotating Camera Thing

---
+++ Assets/Scripts/Cursor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+    [SerializeField] string rotateLeftButton;
+    [SerializeField] string rotateRightButton;
+    [SerializeField] float rotateSpeed = 1;
+
+    float wantedYRot;
+
+    void Update()
+    {
+        if (Input.GetButtonDown(rotateLeftButton))
+            wantedYRot += 90;
+
+        if (Input.GetButtonDown(rotateRightButton))
+            wantedYRot -= 90;
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(transform.rotation.eulerAngles.x, wantedYRot, 0), rotateSpeed * Time.deltaTime);
+    }
+}

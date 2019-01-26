f9c48e4ffd4fce44e05d8f2e7449ab04943e6137
Ben <bengg2015@gmail.com>
2019-01-26 18:53:11 -0500

    Added Ghost

---
+++ Assets/GhostMover.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class GhostMover : MonoBehaviour
+{
+    public Transform cursor;
+
+    private void LateUpdate()
+    {
+        transform.position = cursor.position;
+    }
+}

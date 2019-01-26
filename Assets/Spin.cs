7333cb0256ed545c4816acc61dd3d8b7a255fef3
Ben <bengg2015@gmail.com>
2019-01-26 14:44:01 -0500

    Created Title Screen

---
+++ Assets/Spin.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CameraSpin : MonoBehaviour
+{
+    public float rotateSpeed;
+
+    private void Update()
+    {
+        transform.Rotate(0, rotateSpeed, 0);
+    }
+}

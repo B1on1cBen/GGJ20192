50e360921f05c63442d9b2ca5e2144e4400c835a
pnaatz <41494483+pnaatz@users.noreply.github.com>
2019-01-26 09:14:01 -0500

    ImageFunction

---
+++ Assets/ImageScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageScript : MonoBehaviour {
+
+    bool active = false;
+    public Image img;
+
+    private void Start()
+    {
+            img.gameObject.SetActive(active);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            active = !active;
+            img.gameObject.SetActive(active);
+        }
+	}
+
+}

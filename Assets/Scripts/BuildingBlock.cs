b58b74bbfaebbaab104475230f3dfd74e6e81412
Ben <bengg2015@gmail.com>
2019-01-25 19:11:59 -0500

    Added Helper Classes To Building Block

--- Assets/Scripts/BuildingBlock.cs
+++ Assets/Scripts/BuildingBlock.cs
@@ -1,19 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class BuildingBlock : MonoBehaviour {
-
+public class BuildingBlock : MonoBehaviour
+{
     public GameObject Occupant;
     [HideInInspector] public GameObject[] Borders;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Vector3 GetNeighborPosition(int x, int y)
+    {
+        if (!NeighborExists(x, y))
+            return transform.position;
+
+        return Borders[ConvertXYToNeighborIndex(x, y)].transform.position;
+    }
+
+    private bool NeighborExists(int x, int y)
+    {
+        return Borders[ConvertXYToNeighborIndex(x, y)] != null;
+    }
+
+    private int ConvertXYToNeighborIndex(int x, int y)
+    {
+        if (x == -1)
+            return 2;
+
+        if (x == 1)
+            return 1;
+
+        if (y == -1)
+            return 3;
+
+        if (y == 1)
+            return 0;
+
+        return 0;
+    }
 }
--- Assets/Scripts/Cursor.cs
+++ Assets/Scripts/Cursor.cs
@@ -1,20 +1,88 @@
 ﻿using UnityEngine;
 
+public struct Point
+{
+    public int x;
+    public int y;
+
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
+
 public class Cursor : MonoBehaviour
 {
+    GameManager gameManager;
+    BuildingBlock currentBlock;
+
     [SerializeField] string rotateLeftButton;
     [SerializeField] string rotateRightButton;
-    [SerializeField] float rotateSpeed = 1;
+    [SerializeField] float rotateSpeed = 10;
+    [SerializeField] float moveInterval = .75f;
+    [SerializeField] float moveSpeed = 10;
 
     float wantedYRot;
+    float moveTimer;
+
+    void Awake()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        gameManager.generate();
+        currentBlock = gameManager.topLeft.GetComponent<BuildingBlock>();
+        transform.position = currentBlock.transform.position;
+    }
 
     void Update()
     {
+        Rotate();
+        Point movement = GetMovement();
+        MoveToNewTile(movement);
+    }
+
+    void MoveToNewTile(Point movement)
+    {
+        if (movement.x == 0 && movement.y == 0)
+            return;
+
+        Vector3 wantedPosition = currentBlock.GetNeighborPosition(movement.x, movement.y);
+        if (wantedPosition == currentBlock.transform.position)
+            return;
+
+        transform.position = Vector3.Lerp(transform.position, wantedPosition, Time.deltaTime * moveSpeed);
+    }
+
+    Point GetMovement()
+    {
+        if (moveTimer <= 0)
+        {
+            moveTimer = moveInterval;
+
+            int h = (int)Input.GetAxisRaw("Horizontal");
+            int v = (int)Input.GetAxisRaw("Vertical");
+
+            return new Point(h, v);
+        }
+        else
+        {
+            moveTimer -= Time.deltaTime;
+        }
+
+        return new Point(0, 0);
+    }
+
+    void Rotate()
+    {
         if (Input.GetButtonDown(rotateLeftButton))
+        {
             wantedYRot += 90;
+        }
 
         if (Input.GetButtonDown(rotateRightButton))
+        {
             wantedYRot -= 90;
+        }
 
         transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(transform.rotation.eulerAngles.x, wantedYRot, 0), rotateSpeed * Time.deltaTime);
     }
